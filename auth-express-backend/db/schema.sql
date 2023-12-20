8316b49143190a83510594b9b5397f1fbff2e8ed
DJones <dwaynejones@pursuit.com>
2023-12-19 21:56:47 -0500

    set

--- auth-express-backend/db/schema.sql
+++ auth-express-backend/db/schema.sql
@@ -1,14 +1,12 @@
-
 DROP DATABASE IF EXISTS auth_dev;
 DROP TABLE IF EXISTS users;
-
 
 CREATE DATABASE auth_dev;
 
 \c auth_dev;
 
 CREATE TABLE users (
-  user_id INT PRIMARY KEY AUTO_INCREMENT,
+  user_id SERIAL PRIMARY KEY,
   username VARCHAR(50) NOT NULL,
   email VARCHAR(100) NOT NULL,
   password_hash VARCHAR(255) NOT NULL,
@@ -16,10 +14,11 @@ CREATE TABLE users (
 );
 
 CREATE TABLE registration (
-    resource_id INT PRIMARY KEY ,
+    resource_id SERIAL PRIMARY KEY,
     user_id INT,
     title VARCHAR(100) NOT NULL,
     content TEXT,
     creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
+
--- auth-express-backend/db/seed.sql
+++ auth-express-backend/db/seed.sql
@@ -1,9 +1,9 @@
 \c auth_dev;
 
-INSERT INTO users (firstname, lastname, email, password)
+INSERT INTO users (username, email, password_hash)
 VALUES
-('Gigi', 'Scarborough', 'gigi@me.com', '$2a$10$o5/9VUbgLhN/IEJQ7jRJOO4FZDrL5yENb6GtFrWoAX7r92z7OXeem'),
-('Tim', 'McKiernan', 'tim@me.com', '$2a$10$vJHRxxeVwXo1fAoc.0aRY.kVIWghxlZbkyIwrKoxX1UNYkIGUjQqC'),
-('Dwayne', 'Jones', 'dwayne@me.com', '$2y$10$xoSFj8GqXCB74uGCSbkffuYGKWxQrkHIeTXQhODO3BEwOoaLIMuVG')
-('Aisha', 'Kleemoff', 'aisha@me.com', '$2y$10$zZEPMMExFe.eGNH9m5anRei1mx0o5rYimnKA1/zjbnN.27xZapac.');
-('Michael', 'Kleemoff', 'michael@me.com', '$2y$10$kdR50ECZjjcCJ8LrPp2YPOk4VbVgXC66kDyTdam.Oa7A.6LxmFeRS');
+('Gigi Scarborough', 'gigi@me.com', '$2a$10$o5/9VUbgLhN/IEJQ7jRJOO4FZDrL5yENb6GtFrWoAX7r92z7OXeem'),
+('Tim McKiernan', 'tim@me.com', '$2a$10$vJHRxxeVwXo1fAoc.0aRY.kVIWghxlZbkyIwrKoxX1UNYkIGUjQqC'),
+('Dwayne Jones', 'dwayne@me.com', '$2y$10$xoSFj8GqXCB74uGCSbkffuYGKWxQrkHIeTXQhODO3BEwOoaLIMuVG'),
+('Aisha Kleemoff', 'aisha@me.com', '$2y$10$zZEPMMExFe.eGNH9m5anRei1mx0o5rYimnKA1/zjbnN.27xZapac.'),
+('Michael Kleemoff', 'michael@me.com', '$2y$10$kdR50ECZjjcCJ8LrPp2YPOk4VbVgXC66kDyTdam.Oa7A.6LxmFeRS');
